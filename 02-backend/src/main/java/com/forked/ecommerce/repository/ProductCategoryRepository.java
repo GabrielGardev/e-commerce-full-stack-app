4d9459ca2c86bdcba2bfe19107c0c5509504f08c
GabrielGardev <gabriel.gurdev@gmail.com>
2020-08-26 19:10:01 +0300

    add cross origin

--- 02-backend/src/main/java/com/forked/ecommerce/repository/ProductCategoryRepository.java
+++ 02-backend/src/main/java/com/forked/ecommerce/repository/ProductCategoryRepository.java
@@ -1,10 +1,12 @@
-package com.forked.ecommerce.dao;
+package com.forked.ecommerce.repository;
 
 import com.forked.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 //name of JSON entry and /product-category path
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- 02-backend/src/main/java/com/forked/ecommerce/repository/ProductRepository.java
+++ 02-backend/src/main/java/com/forked/ecommerce/repository/ProductRepository.java
@@ -1,7 +1,9 @@
-package com.forked.ecommerce.dao;
+package com.forked.ecommerce.repository;
 
 import com.forked.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
