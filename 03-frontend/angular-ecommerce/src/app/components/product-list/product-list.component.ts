a782ec7ed9d6a2b0258694589421e1b4b0d9f9f3
GabrielGardev <gabriel.gurdev@gmail.com>
2020-08-27 14:21:28 +0300

    add dynamic categories and searching

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
 
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
@@ -29,9 +30,13 @@ export class ProductListComponent implements OnInit {
     if (hasCategoryId) {
       //get the 'id' string. Convert string to a number using Number class
       this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
+
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
     } else {
       //not category 'id' available ... default to category 'id' 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     // now get the products for given category id
