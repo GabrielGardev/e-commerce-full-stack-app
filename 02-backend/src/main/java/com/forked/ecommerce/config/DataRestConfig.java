20f826ed8acb9d84f8d74932535f74af0b75f38c
GabrielGardev <gabriel.gurdev@gmail.com>
2020-08-27 12:58:39 +0300

    add more functionality

--- 02-backend/src/main/java/com/forked/ecommerce/config/DataRestConfig.java
+++ 02-backend/src/main/java/com/forked/ecommerce/config/DataRestConfig.java
@@ -2,13 +2,28 @@ package com.forked.ecommerce.config;
 
 import com.forked.ecommerce.entity.Product;
 import com.forked.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         HttpMethod[] unsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
@@ -24,5 +39,29 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        //call an internal helper method to expose ids for categories
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        //expose ids
+
+        //get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        //crate an array list of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        //get the entity types for the entities
+        for (EntityType entity : entities) {
+            entityClasses.add(entity.getJavaType());
+        }
+
+        //expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = new Class[entityClasses.size()];
+        domainTypes = entityClasses.toArray(domainTypes);
+
+        config.exposeIdsFor(domainTypes);
     }
 }
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,25 +3,42 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClien: HttpClient) { }
 
-  getProductList(): Observable<Product[]>{
-    return this.httpClien.get<GetRespons>(this.baseUrl).pipe(
+  getProductList(categoryId: number): Observable<Product[]> {
+
+    //need to build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+
+    return this.httpClien.get<GetResponsProduct>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClien.get<GetResponsProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetRespons {
+interface GetResponsProduct {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponsProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
